5bb95748a478ba68ec2572eda0c5f906ea5d97d1
John-Rayson <marsonia.rayson@gmail.com>
2023-03-18 03:05:10 +0800

    Minor update for the code "main.py"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from typing import Union
-
+from typing import Union, List
 from fastapi import FastAPI
 from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
 
 app = FastAPI()
 
@@ -11,17 +12,35 @@ class Item(BaseModel):
     price: float
     is_offer: Union[bool, None] = None
 
+class UserName(str, Enum):
+    robert = "robert"
+    lance = "lance"
+    eugine = "eugine"
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+
+@app.get("/users/{user_name}")
+async def get_username(user_name: UserName):
+    if user_name is UserName.robert:
+        return {"user_name": user_name, "message": "HELLO MY NAME IS ROBERT"}
+
+    if user_name.value == "lance":
+        return {"user_name": user_name, "message": "YOOO! AKO SI LANCE"}
+
+    return {"user_name": user_name, "message": "Kon'nichiwa, watashinonamaeha yujindesu"}
 
 
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+
+###@app.get("/items/{item_id}")
+###def read_item(item_id: int, q: Union[str, None] = None):
+###    return {"item_id": item_id, "q": q}
+
+
+###@app.put("/items/{item_id}")
+###def update_item(item_id: int, item: Item):
+ ###   return {"item_name": item.name, "item_id": item_id}
